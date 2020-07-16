8e16d379fdb0b5d3da7d02b03ce8e77203f303db
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-17 00:50:44 +0530

    add side notes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/card-list/card-list.component'
 
 class App extends Component {
 
+  // Remember to put constructor in the class which holds the super function.
   constructor(props) {
     super();
 
@@ -12,6 +13,8 @@ class App extends Component {
     };
   }
 
+  // Gets executed every time the component is mounted. 
+  // To check if state is updated use componentDidUpdate? function.
   componentDidMount() {
     fetch('http://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
