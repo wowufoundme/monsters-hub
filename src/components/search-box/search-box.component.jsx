b40ac74632310d091601729f73788e8270ffe42c
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-20 18:43:10 +0530

    add search box component and init

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './search-box.styles.css';
 
-const SearchBox = props => {
-    return(
-        <div>
-            <input />
-        </div>
-    )
-};
+class SearchBox extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            searchField : ''
+        }
+    }
+
+    render() {
+        return(
+            <div className='search-container'>
+                <input 
+                    type='search'
+                    placeholder='Search Monsters'
+                    className='search-bar'
+                    value={this.state.searchField}
+                    onChange={searchText => this.setState({ searchField: searchText.target.value })}
+                />
+            </div>
+        )
+    }
+}
 
 export default SearchBox;
