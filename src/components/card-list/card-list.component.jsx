cc1843e28fbb74d731b68d95493989755e3b0cbb
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-20 18:05:44 +0530

    add key to list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ const CardList = props => {
     return (
         <div className="card-list">
             {props.monsters.map(monster => (
-                <Card monster={monster} />
+                <Card key={monster.id} monster={monster} />
             ))}
         </div>
     );
