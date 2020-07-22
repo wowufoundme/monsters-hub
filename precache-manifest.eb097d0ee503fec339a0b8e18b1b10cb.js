2a7df2a782dc9412d1fe7912d8f9c39c47c20356
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-07-22 20:35:44 +0530

    Updates

---
+++ precache-manifest.eb097d0ee503fec339a0b8e18b1b10cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df09a278561a1fede3897af039616c3",
+    "url": "/monsters-hub/index.html"
+  },
+  {
+    "revision": "457b954bb08b154c4828",
+    "url": "/monsters-hub/static/css/main.f74e2ea5.chunk.css"
+  },
+  {
+    "revision": "21048a440eb1c54c441f",
+    "url": "/monsters-hub/static/js/2.34917af1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-hub/static/js/2.34917af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457b954bb08b154c4828",
+    "url": "/monsters-hub/static/js/main.5149a985.chunk.js"
+  },
+  {
+    "revision": "fb362679d1f6ddc805de",
+    "url": "/monsters-hub/static/js/runtime-main.f183e61b.js"
+  }
+]);
